c3646630f352ee58dc6b2f9946f2cc0d88dc7da9
lambchop96 <justinkalam@gmail.com>
2022-11-10 18:12:04 -0800

    Completed lab

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,34 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test(timeout = 4000)
+  public void testFilter() {
+    StringChecker nonempty = (String s) -> !s.isEmpty();
+    ArrayList<String> input1 = new ArrayList<String>();
+    Assert.assertEquals(input1, ListExamples.filter(input1, nonempty));
+    input1.add("a");
+    Assert.assertEquals(input1, ListExamples.filter(input1, nonempty));
+    input1.add("b");
+    Assert.assertEquals(input1, ListExamples.filter(input1, nonempty));
+    input1.add("");
+    Assert.assertEquals(
+        new ArrayList<String>(Arrays.asList("a", "b")), ListExamples.filter(input1, nonempty));
+  }
+
+  @Test(timeout = 4000)
+  public void testMerge() {
+    ArrayList<String> input1 = new ArrayList<>(Arrays.asList("a"));
+    ArrayList<String> input2 = new ArrayList<>(Arrays.asList("b", "c", "d"));
+    ArrayList<String> test = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+    Assert.assertEquals(test, ListExamples.merge(input1, input2));
+    input1 = new ArrayList<>(Arrays.asList("a", "b", "c"));
+    input2 = new ArrayList<>(Arrays.asList("d"));
+    test = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+    Assert.assertEquals(test, ListExamples.merge(input1, input2));
+    Assert.assertEquals(
+        new ArrayList<String>(), ListExamples.merge(new ArrayList<>(), new ArrayList<>()));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -4,4 +4,41 @@ set -e
 
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission/
+FILE=ListExamples.java
 
+if [[ -f "$FILE" ]]
+then
+        echo "ListExamples.java exists!"
+else
+        echo "Cannot find ListExamples.java!"
+        echo "Your score is 0 out of 3!"
+        exit
+fi
+
+cp ../TestListExamples.java ./
+
+set +e
+
+SCORE=0
+
+javac -cp ".;../lib/*" ListExamples.java TestListExamples.java
+
+if [[ $? -eq 0 ]]
+then
+  SCORE=$(($SCORE+1))
+else
+  echo "Your score is" $SCORE "out of 3!"
+  exit
+fi
+
+FAILED=$(java -cp ".;../lib/*" org.junit.runner.JUnitCore TestListExamples | grep -oP "(?<=,  Failures: )[0-9]+")
+
+if [[ $? -eq 1 ]] # passed all tests
+then
+  SCORE=$(($SCORE+2))
+else
+  SCORE=$(($SCORE+2-$FAILED))
+fi
+
+echo "Your score is" $SCORE "out of 3!"
